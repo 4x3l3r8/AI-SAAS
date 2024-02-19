8bd50cb7183650718984aa5e4c2ec6b02368b672
Daniel Adesanya <dcadesanya@sidmach.com>
2024-02-19 16:43:53 +0100

    feat: layout complete

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,11 @@
-import { Sidebar } from '@/components/shared'
+import { MobileNav, Sidebar } from '@/components/shared'
 import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className='root'>
             <Sidebar />
-            {/* <MobileNav/> */}
+            <MobileNav />
             <div className="root-container">
                 <div className="wrapper">
                     {children}
